f58c118f181c92a2361261a69310dea0d2d66e8b
KAI ZHAO <kaiz.xyz@gmail.com>
2018-06-06 16:57:44 +0800

    fix

--- install-nccl.sh
+++ install-nccl.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 cd /tmp && git clone https://github.com/NVIDIA/nccl && cd nccl
 make CUDA_HOME=/usr/local/cuda test -j$(nproc)
-if [ $? == 1 ]; then
-    sudo make install -j$(nproc)
-else
-    echo "test failed!"
-    exit 1
-fi
+sudo make install -j$(nproc)
 echo "All done, nccl has been installed to /usr/local'"
